d81778919032d7c8ab16bb7c614136ff9b315658
Joshua Higginbotham <jhigginbotham64@gmail.com>
2022-04-15 22:14:43 -0500

    derp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from nlp_project import *
 
-model = create_text_generator()
+model = create_text_generator(num_training_epochs=20)
 
-print(generate_text(model, "Moon"))
+print(generate_text(model, "Antifa calls for"))
               
 # GAN Loss
 # Preprocessing using NLTK/Spacy
