1494aa775af5fd93113787be23dba9047e3b6d81
Joshua Higginbotham <jhigginbotham64@gmail.com>
2022-04-15 21:55:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import nlp_project
 
 model = create_text_generator()
 
-generate_text(model, "Moon")
+print(generate_text(model, "Moon"))
               
 # GAN Loss
 # Preprocessing using NLTK/Spacy
