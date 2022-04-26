049f96c2b6665ecd849002846202d0e71b3729f8
Sarah Beaver <sbeaver57@yahoo.com>
2022-04-25 21:15:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ model = create_text_generator(num_training_epochs=20)
 
 print(generate_text(model, "Antifa calls for"))
 
-model2 = create_text_generatorLSTM(num_training_epochs=5)
+model2 = create_text_generatorLSTM(num_training_epochs=20)
 
 print(generate_text(model2, "Antifa calls for"))
+
+model3 = create_text_generatorLSTM(num_training_epochs=20, num_layers=2)
+
+print(generate_text(model3, "Antifa calls for"))
+
+
+
               
 # GAN Loss
 # Preprocessing using NLTK/Spacy
